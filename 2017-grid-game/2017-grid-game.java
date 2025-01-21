e61de064a592cde54c278eeb94550a2d524e7521
Bandari Ramcharan <118244554+Ramcharan05@users.noreply.github.com>
2025-01-21 21:03:54 +0530

    Time: 12 ms (8.81%), Space: 58.2 MB (10.36%) - LeetHub

--- 2017-grid-game/2017-grid-game.java
+++ 2017-grid-game/2017-grid-game.java
@@ -5,12 +5,16 @@ class Solution {
             sum+=grid[0][i];
         }
         System.out.println("Sum :"+sum);
-        long c=0;
+        long c1=0;
+        long c2=0;
         long res=Long.MAX_VALUE;
         for(int i=0;i<grid[0].length;i++){
-            sum-=grid[0][i];
-            res=Math.min(res,Math.max(sum,c));
-            c+=grid[1][i];
+            c1+=grid[0][i];
+            long t=sum-c1;
+            if(i>0){
+                c2+=grid[1][i-1];
+            }
+            res=Math.min(res,Math.max(t,c2));
         }
         return res;
     }
